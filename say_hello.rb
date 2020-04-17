2918138a487da490e1f5954b85b2edc27aa95101
Cailie Skelton <cskelton707@gmail.com>
2020-04-17 02:41:42 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
-# Build your say_hello method here
+def greeting(name=John)
+  puts "Hello, #{name}!"
+end
+
+greeting("John")
+
+def greeting_with_language(name="Ruby Programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you were a great #{language} programmer!"
+end
+
+greeting_with_language("Celeste", "Elixer")
